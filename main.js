64e8338870d7ff608100df76fb65ff7004c0d0e8
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-01-18 17:03:45 +0800

    penambahan fitur tandai dan belum ditandai sebagai dibaca

--- main.js
+++ main.js
@@ -30,14 +30,14 @@ function belumDibaca(load) {
     let dataBelumDibaca = document.getElementById("dataBelumDibaca");
     if (load === undefined) {
         if (db[db.length - 1].sudahDibaca === false) {
-            let html = `<tr> <th>Judul</th> <td>:</td> <td>${db[db.length-1].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[db.length-1].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[db.length-1].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="">Tandai Sudah Dibaca</button></td> </tr> <tr> <td><button style="font-size: 10px; background-color:red;" onclick="delBelumDibaca(${db[db.length-1].idBook})">Hapus</button></td> </tr>`;
+            let html = `<tr> <th>Judul</th> <td>:</td> <td>${db[db.length-1].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[db.length-1].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[db.length-1].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="belumToSudah(${db[db.length-1].idBook})">Tandai Sudah Dibaca</button></td> </tr> <tr> <td><button style="font-size: 10px; background-color:red;" onclick="delBelumDibaca(${db[db.length-1].idBook})">Hapus</button></td> </tr>`;
             dataBelumDibaca.innerHTML += html
         }
     } else {
         let temp = ""
         for (let data in db) {
             if (db[data].sudahDibaca === false) {
-                temp += `<tr> <th>Judul</th> <td>:</td> <td>${db[data].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[data].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[data].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="">Tandai Sudah Dibaca</button></td> </tr> <tr> <td><button style="font-size: 10px; background-color:red;" onclick="delBelumDibaca(${db[data].idBook})">Hapus</button></td> </tr>`;
+                temp += `<tr> <th>Judul</th> <td>:</td> <td>${db[data].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[data].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[data].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="belumToSudah(${db[data].idBook})">Tandai Sudah Dibaca</button></td> </tr> <tr> <td><button style="font-size: 10px; background-color:red;" onclick="delBelumDibaca(${db[data].idBook})">Hapus</button></td> </tr>`;
             }
         }
         dataBelumDibaca.innerHTML = temp
@@ -48,14 +48,14 @@ function telahDibaca(load) {
     let dataSudahDibaca = document.getElementById("dataSudahDibaca");
     if (load === undefined) {
         if (db[db.length - 1].sudahDibaca === true) {
-            let html = `<tr> <th>Judul</th> <td>:</td> <td>${db[db.length-1].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[db.length-1].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[db.length-1].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;">Tandai Belum Dibaca</button></td> </tr><tr> <td><button style="font-size: 10px; background-color:red;" onclick="delSudahDibaca(${db[db.length-1].idBook})">Hapus</button></td> </tr>`;
+            let html = `<tr> <th>Judul</th> <td>:</td> <td>${db[db.length-1].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[db.length-1].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[db.length-1].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="sudahToBelum(${db[db.length-1].idBook})">Tandai Belum Dibaca</button></td> </tr><tr> <td><button style="font-size: 10px; background-color:red;" onclick="delSudahDibaca(${db[db.length-1].idBook})">Hapus</button></td> </tr>`;
             dataSudahDibaca.innerHTML += html
         }
     } else {
         let temp = ""
         for (let data in db) {
             if (db[data].sudahDibaca === true) {
-                temp += `<tr> <th>Judul</th> <td>:</td> <td>${db[data].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[data].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[data].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="">Tandai Sudah Dibaca</button></td> </tr> <tr> <td><button style="font-size: 10px; background-color:red;" onclick="delBelumDibaca(${db[data].idBook})">Hapus</button></td> </tr>`;
+                temp += `<tr> <th>Judul</th> <td>:</td> <td>${db[data].judul}</td> </tr> <tr> <th>Penulis</th> <td>:</td> <td>${db[data].penulis}</td> </tr> <tr> <th>Tahun Terbit</th> <td>:</td> <td>${db[data].tahunTerbit}</td> </tr> <tr> <td><button style="font-size: 10px;" onclick="sudahToBelum(${db[data].idBook})">Tandai Belum Dibaca</button></td> </tr> <tr> <td><button style="font-size: 10px; background-color:red;" onclick="delSudahDibaca(${db[data].idBook})">Hapus</button></td> </tr>`;
             }
         }
         dataSudahDibaca.innerHTML = temp
@@ -81,5 +81,27 @@ function delSudahDibaca(iddelSudahDibaca) {
     }
     alert("Data Dihapus!")
     telahDibaca("load");
+    return db
+}
+
+function belumToSudah(idBelumToSudah){
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].idBook === idBelumToSudah) {
+            db[i].sudahDibaca = true
+        }
+    }
+    belumDibaca("load");
+    telahDibaca("load");
+    return db
+}
+
+function sudahToBelum(idSudahToBelum){
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].idBook === idSudahToBelum) {
+            db[i].sudahDibaca = false
+        }
+    }
+    telahDibaca("load");
+    belumDibaca("load");
     return db
 }
